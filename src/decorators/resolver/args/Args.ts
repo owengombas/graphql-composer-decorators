cbde6a92dce91e25ee37ce896392554333f59037
owen <owen@venmac.local>
2020-06-17 01:55:11 +0200

    improvement(Args): type function

--- src/decorators/resolver/args/Args.ts
+++ src/decorators/resolver/args/Args.ts
@@ -1,16 +1,20 @@
 import { Args as ComposerArgs } from "graphql-composer";
-import { MetadataStorage, ExtensionsType } from "../../..";
+import { MetadataStorage, ExtensionsType, TypeFunction } from "../../..";
 
-export function Args() {
+export function Args();
+export function Args(type: TypeFunction);
+export function Args(type?: TypeFunction) {
   return (target: Object, key: string, index: number) => {
+    const typeFn = () =>
+      Reflect.getMetadata("design:paramtypes", target, key)[index];
+
     const meta: ExtensionsType = {
       decoratorInfos: {
         classType: target.constructor,
         key,
         kind: "flat-args",
         index,
-        type: () =>
-          Reflect.getMetadata("design:paramtypes", target, key)[index],
+        type: type || typeFn,
         params: {},
       },
     };
